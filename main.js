5bcce2fa2c5ca90923ac8edc4f6a621cce6ddd1c
Ihor Kolesnikov <igorkolesnikov2323@gmail.com>
2026-04-09 12:52:17 +0200

    my second Project, Todo list

---
+++ main.js
@@ -0,0 +1,51 @@
+const input = document.querySelector('#input')
+const inputButton = document.querySelector('.input__button')
+const tasksList = document.querySelector('.tasks__list')
+let text = input.value
+const inputBox = document.querySelector('.input__box')
+let flag = false
+let errorText = document.createElement('h1')
+
+
+inputButton.addEventListener('click', function(event){
+    if (!input.value == ''){
+        errorText.remove()
+
+        let newTask = document.createElement('li')   
+        newTask.innerText = input.value
+        newTask.className = 'tasks__item'
+        tasksList.append(newTask)
+
+        input.value = ""
+
+        let completeButton = document.createElement('input')
+        completeButton.type = 'checkbox'
+        completeButton.className = 'complete__btn'
+        newTask.append(completeButton)
+        completeButton.addEventListener('click', function(){
+            completeButton.classList.toggle('complete__btn2')
+            newTask.classList.toggle('tasks__item2')
+            newTask.classList.toggle('completeting')
+        })
+
+        let removeButton = document.createElement('span')
+        removeButton.innerText = "✖"
+        removeButton.className = 'remove__btn'
+        newTask.append(removeButton)
+        console.log(removeButton)
+        removeButton.addEventListener('click', function(){
+            newTask.classList.remove('completeting')
+            setTimeout(() => {
+                newTask.remove()
+            }, 500);
+            newTask.classList.add ('removing')
+        })
+
+    } else {
+        errorText.innerText = 'Oops, please enter name item'
+        errorText.className = 'errortext'
+        tasksList.append(errorText)
+        console.log(errorText)
+    }
+})
+
